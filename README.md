47dec7671dfefa8a246dfc37f8bd66040522e404
= <=>
2014-01-30 09:18:57 -0700

    Updated README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a redo of Michael Harl's tutorial just so that I can implant the things taught in the tutorial deep into the skull.
+Basically a twitter app.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MichaelHartlRedo::Application.config.secret_key_base = 'ed8ac8a5f28d929542ffe557417da8eaf085e9438f09c39c86617420d32af7181b177cb627c584e663f6d35214db73eeb66a55e4fafac5550504f5233e5740a3'
