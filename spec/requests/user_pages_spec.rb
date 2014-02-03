bbcb57702e73866aa6de97898ca9f6f7ee11ec60
Tom Taila <tom.taila@evogi.com>
2014-02-03 15:57:06 -0700

    Finish layouts and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+include ApplicationHelper
+
+describe "UserPages" do
+
+  subject{page}
+
+  describe "Sign up page" do
+    before{visit signup_path}
+    it{should have_title(full_title('Sign up'))}
+    it{should have_content('Sign up')}
+  end
+
+end
